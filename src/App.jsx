ee2bfb388f990b6b9b5d377f906fb9aa68377641
nandhini0609 <nandhiniprabu0608@gmail.com>
2026-03-19 17:11:57 +0530

    Moved tracking page from the startingcode

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckOut } from './pages/checkoutPage'
 import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckOut />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
 
   )
