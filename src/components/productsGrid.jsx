9681009e0e35139ddb15e79c78f0f7338b966396
nandhini0609 <nandhiniprabu0608@gmail.com>
2026-04-08 18:07:38 +0530

    created data in the backend by using post method

--- src/components/productsGrid.jsx
+++ src/components/productsGrid.jsx
@@ -1,64 +1,13 @@
-import axios from "axios"
-import { formatMoney } from '../utils/money';
+import { Product } from "./product";
 export function ProductsGrid({ products, loadCart }) {
+
     return (
         <div className="products-grid">
             <div className="product-container">
                 {products.map((product) => {
                     return (
-                        <div className="product-container" key={product.id}>
-                            <div className="product-image-container">
-                                <img className="product-image" src={product.image} />
-                            </div>
+                        <Product key={product.id} product={product} loadCart={loadCart} />
 
-                            <div className="product-name limit-text-to-2-lines">
-                                {product.name}
-                            </div>
-
-                            <div className="product-rating-container">
-                                <img
-                                    className="product-rating-stars"
-                                    src={`/images/ratings/rating-${product.rating.stars * 10}.png`}
-                                />
-                                <div className="product-rating-count link-primary">
-                                    {product.rating.count}
-                                </div>
-                            </div>
-
-                            <div className="product-price">
-                                {formatMoney(product.priceCents)}
-                            </div>
-
-                            <div className="product-quantity-container">
-                                <select>
-                                    {[...Array(10)].map((_, i) => (
-                                        <option key={i + 1} value={i + 1}>
-                                            {i + 1}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-
-                            <div className="product-spacer"></div>
-
-                            <div className="added-to-cart">
-                                <img src="/images/icons/checkmark.png" />
-                                Added
-                            </div>
-
-                            <button className="add-to-cart-button button-primary" onClick={async () => {
-                                await axios.post("/api/cart-items", {
-                                    productId: product.id,
-                                    quantity: 1
-                                })
-                                await loadCart();
-
-                            }
-
-                            }>
-                                Add to Cart
-                            </button>
-                        </div>
                     );
                 })}
 
